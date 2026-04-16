d458029b4ddc7ececb543781811e25c0b92a96a3
Gautamikahar <gautami.kahar@gmail.com>
2026-04-16 12:37:29 +0530

    Added user-wise issue tracking API

--- issue-service/index.js
+++ issue-service/index.js
@@ -4,17 +4,42 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+let userIssues = {}; 
 let issues = [];
 let id = 1;
 
+// app.post("/issue", (req, res) => {
+//   const issue = { id: id++, ...req.body, status: "pending" };
+//   issues.push(issue);
+//   res.send(issue);
+// });
+
 app.post("/issue", (req, res) => {
-  const issue = { id: id++, ...req.body, status: "pending" };
-  issues.push(issue);
-  res.send(issue);
+  const { title, username } = req.body;
+
+  const issue = {
+    id: Date.now(),
+    title,
+    status: "pending",
+    username
+  };
+
+  if (!userIssues[username]) {
+    userIssues[username] = [];
+  }
+
+  userIssues[username].push(issue);
+
+  res.json(issue);
 });
 
-app.get("/issues", (req, res) => {
-  res.json(issues);
+// app.get("/issues", (req, res) => {
+//   res.json(issues);
+// });
+
+app.get("/user/:username/issues", (req, res) => {
+  const username = req.params.username;
+  res.json(userIssues[username] || []);
 });
 
 app.listen(3001, () => {
